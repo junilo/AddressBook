9dd892863e784a234166de8c9c9afbf9a78a91b9
junilo <jnl.pagobo@gmail.com>
2024-06-29 13:38:49 +1000

    fix: IContactService dependency injection

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,7 @@
 using API;
 using API.Models;
 using API.Services;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,6 +20,7 @@ builder.Services.AddCors(options =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddScoped<IContactService, ContactService>();
 
 var app = builder.Build();
 
@@ -42,12 +44,12 @@ app.MapGet("/contact/{id}", async (IContactService contactService, int id) =>
     return await contactService.GetContact(id);
 });
 
-app.MapPost("/contact", async (IContactService contactService, Contact request) =>
+app.MapPost("/contact", async (IContactService contactService, [FromBody] Contact request) =>
 {
     return await contactService.AddContact(request);
 });
 
-app.MapPut("/contact/{id}", async (IContactService contactService, int id, Contact request) =>
+app.MapPut("/contact/{id}", async (IContactService contactService, int id, [FromBody] Contact request) =>
 {
     request.Id = id;
     return await contactService.UpdateContact(request);
